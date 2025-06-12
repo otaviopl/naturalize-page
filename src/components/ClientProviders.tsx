a9e8b1b0aa042d84cd1b5293d2ed61e53453cc0a
otaviopl <otavio.pereira.lopes@gmail.com>
2025-06-12 19:22:40 -0300

    fix: remove WhatsAppWidget import in ClientProviders to clean up component

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -5,7 +5,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
 import dynamic from 'next/dynamic';
 
-const WhatsAppWidget = dynamic(() => import('./WhatsAppWidget'), { ssr: false });
 const InstagramWidget = dynamic(() => import('./InstagramWidget'), { ssr: false });
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
